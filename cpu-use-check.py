fcd11f832ecdbe9319886cf29f858308a69f1779
ini-i <137917587+ini-i@users.noreply.github.com>
2023-06-27 12:46:41 -0400

    Update cpu-use-check.py

--- cpu-use-check.py
+++ cpu-use-check.py
@@ -3,5 +3,5 @@ import psutil
 #this checks the CPU usage in a user inputted interval
 
 def cpu_usage(time):
-  cpu_use = psutil.cpu_percent(time)
-  print("The CPU usage for {} seconds is {}".format(time, cpu_use))
+  cpu_percent = psutil.cpu_percent(time)
+  print("The CPU usage for {} seconds is {}%".format(time, cpu_percent))
