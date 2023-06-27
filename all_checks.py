9206e7cc768ed7571587c2258a56e62ca02a9f05
iniog <ini.ogunsanya18@gmail.com>
2023-06-27 13:34:26 -0400

    Create wrapper function for check_disk_full

--- all_checks.py
+++ all_checks.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 import os
 import sys
-import psutil
+import shutil
 
 def check_reboot():
     """Returns True if the computer has a pending reboot"""
@@ -18,13 +18,18 @@ def check_disk_full(disk, min_gb, min_percent):
         return True
     return False
 
+def check_root_full():
+    """Returns True if the root partitiion is full, False otherwise"""
+    return check_disk_full(disk="/", min_gb=2, min_percent=10)
+
 def main():
     if check_reboot():
         print("Pending Reboot.")
         sys.exit(1)
-    if check_disk_full(disk="/", min_gb=2, min_percent=10):
-        print("Disk full")
+    if check_root_full():
+        print("Root partitiion full")
         sys.exit(1)
+
     print("Everything okay")
     sys.exit(0)
 main()
